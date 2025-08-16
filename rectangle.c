fbd3b41132ddad1936fb66b01a7f0f3411541007
Shivam Kumar Yadav <sk4262@srmist.edu.in>
2025-08-16 10:39:18 +0530

    rectangleformula.c

---
+++ rectangle.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main(){
+    int length,breadth;
+    printf("enter the length=");
+    scanf("%d",&length);
+    printf("enter the breadth=");
+    scanf("%d",&breadth);
+    printf("the area of the rectangle=%d",length*breadth);
+    return 0;
+}
