958c9904529079c7f045276abbf057ac2dd3fd18
Shivam Kumar Yadav <sk4262@srmist.edu.in>
2025-08-16 12:13:53 +0530

    dontcalcuationprintf

--- circle.c
+++ circle.c
@@ -1,6 +1,7 @@
 #include<stdio.h>
 int main(){
     int r=6;
-    printf("the area of circle of radius %d is %f",r,3.14*r*r);
+    float a=3.14*r*r;
+    printf("the area of circle of radius %d is %f",r,a);
     return 0;
 }
