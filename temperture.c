fceebaf7f95ffb86b60857cdd019b534eb7b48ee
Shivam Kumar Yadav <sk4262@srmist.edu.in>
2025-08-16 12:04:06 +0530

    change

--- temperture.c
+++ temperture.c
@@ -1,9 +1,9 @@
 #include<stdio.h>
 int main(){
-    float c,f;
+    float c=32.2,f;
     f=(9/5)*c+32;
     scanf(" enter the celsius%f",&c);
-    printf("the value in fahrenheit is %f",&f);
+    printf("the value in fahrenheit is %f",f);
     return 0;
 
 }
