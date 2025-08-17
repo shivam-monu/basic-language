b6db6ef6fcc4ccced504cb10a490f7295d29ac2f
Shivam Kumar Yadav <sk4262@srmist.edu.in>
2025-08-17 11:54:53 +0530

    change

--- arithmaticsopertor.c
+++ arithmaticsopertor.c
@@ -4,4 +4,5 @@ int main(){
     int b=5;
     int c=a+b;
     printf("the value of a is %d and value of b is %d and sum is %d",a,b,c);
+    printf("the remainder when a is divided by is :%d",a%b);//modulus operator is used to get the remainder
 }
