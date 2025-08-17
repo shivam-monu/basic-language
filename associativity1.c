88f6e0bdb80cecda366c3c0e2662aed661a01292
Shivam Kumar Yadav <sk4262@srmist.edu.in>
2025-08-17 23:44:20 +0530

    question 1

---
+++ associativity1.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main(){
+    int a=7;
+    int b=8;
+    int c=5;
+    printf("the value is %d\n",3*b/2*c+7*a);
+    // 3*b/2*c+7*a
+    // 3*b/2*c+49
+    // 24/2*c+49
+    // 12*c+49
+    // 60+49
+    // 109
+}
