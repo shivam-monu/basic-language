5f9744b6208778d94d107302623c89c656760ba7
Shivam Kumar Yadav <sk4262@srmist.edu.in>
2025-08-18 22:37:05 +0530

    if condition

---
+++ conditional.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main()
+{
+    int age = 15;
+    if (age > 10)
+    {
+        printf("we are inside if\n");
+        printf("your age is greater than 10\n");
+    }
+    if (age % 5 == 0)
+    {
+        printf("we are inside if\n");
+        printf("your age is greater than 10\n");
+    }
+    return 0;
+}
