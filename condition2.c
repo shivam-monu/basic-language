538f52f3fba380a8d22c84c9579b7dba07212406
Shivam Kumar Yadav <sk4262@srmist.edu.in>
2025-08-18 22:46:50 +0530

    else condition

---
+++ condition2.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main(){
+    int age=5;
+    if(age>10){
+        printf("the age is greater than 10\n");
+    }
+    else{
+        printf("the age is not greater than 10\n");
+    }
+    return 0;
+}
