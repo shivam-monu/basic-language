dd836c3310efe5220da67f3bcd28b48af7997fc4
Shivam Kumar Yadav <sk4262@srmist.edu.in>
2025-08-19 00:10:20 +0530

    add a not logical operator

--- logical.c
+++ logical.c
@@ -3,5 +3,6 @@ int main(){
     int a=1,b=0;
     printf("the value of a and b is%d\n",a&&b);
     printf("the value of a or b is%d\n",a||b);
+    printf("the value of not(a)%d",!a);
     return 0;
 }
